6a7586412fc860ceae273840bc9db7535b75db1b
Byeong Min Cho <merrybmc@gmail.com>
2023-10-17 10:31:19 +0900

    not key commit

--- .eslintrc.js
+++ .eslintrc.js
@@ -10,7 +10,7 @@ module.exports = {
     // 'no-console': ['error', {allow: ['warn', 'error', 'info']}], // console 금지
     eqeqeq: 'error', // 일치연산자 사용 필수
     'react/jsx-pascal-case': 'error', // 컴포넌트 이름 pascal-case 사용
-    'react/jsx-key': 'error', // 반복 요소에 key 사용
+    // 'react/jsx-key': 'error', // 반복 요소에 key 사용
     'dot-notation': 'error', // dot-notation 사용
     'linebreak-style': 0 // LF, CRLF 문제 해결
   }
--- src/App.js
+++ src/App.js
@@ -6,18 +6,15 @@ function App() {
   console.log(name);
   console.log(data);
 
-  let abcde = 2;
-
-  let abcd = 1;
-
-  if (abcde === abcd) {
-    return;
-  }
+  let list = [{name: 'abc'}, {name: 'bcd'}];
   return (
     <>
       {name}
       {data.name}
       {data.age}
+      {list.map((data) => (
+        <p>{data.name}</p>
+      ))}
       <div className='App' onClick={(e) => {}} onMouseDown={() => {}} onMouseEnter={() => {}}>
         안녕하세요
       </div>
