d5b7cc23a746068e850c5b04818a4646caf9617e
Byeong Min Cho <merrybmc@gmail.com>
2023-10-17 10:23:33 +0900

    test

--- src/App.js
+++ src/App.js
@@ -1,16 +1,22 @@
 import {useState} from 'react';
+import data from './test.json';
 
 function App() {
-  const name = useState(1);
+  const [name] = useState(1);
+  console.log(name);
+  console.log(data);
 
+  let abc = 1;
+
+  let abcd = 1;
+
+  if (abc == abcd) {
+  }
   return (
     <>
       {name}
-      {name}
-      {name}
-      {name}
-      {name}
-
+      {data.name}
+      {data.age}
       <div className='App' onClick={(e) => {}} onMouseDown={() => {}} onMouseEnter={() => {}}>
         안녕하세요
       </div>
